91dc9b4cfca1866ed9a45cfe250d22304a6ed99c
Nasr Souid <91200028+souid-nasr@users.noreply.github.com>
2023-05-22 16:50:54 +0100

    portfolio-vhanges

--- app/components/Counts.jsx
+++ app/components/Counts.jsx
@@ -1,60 +1,100 @@
-import React from 'react'
+import React from "react";
 
 function Counts() {
   return (
     <div>
-            <section id="counts" className="counts">
-      <div className="container" data-aos="fade-up">
-
-        <div className="row no-gutters">
-          <div className="image col-xl-5 d-flex align-items-stretch justify-content-center justify-content-lg-start" data-aos="fade-right" data-aos-delay="100"></div>
-          <div className="col-xl-7 ps-0 ps-lg-5 pe-lg-1 d-flex align-items-stretch" data-aos="fade-left" data-aos-delay="100">
-            <div className="content d-flex flex-column justify-content-center">
-              <h3>Voluptatem dignissimos provident quasi</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
-              </p>
-              <div className="row">
-                <div className="col-md-6 d-md-flex align-items-md-stretch">
-                  <div className="count-box">
-                    <i className="bi bi-emoji-smile"></i>
-                    <span data-purecounter-start="0" data-purecounter-end="65" data-purecounter-duration="2" className="purecounter"></span>
-                    <p><strong>Happy Clients</strong> consequuntur voluptas nostrum aliquid ipsam architecto ut.</p>
+      <section id="counts" className="counts">
+        <div className="container" data-aos="fade-up">
+          <div className="row no-gutters">
+            <div
+              className="image col-xl-5 d-flex align-items-stretch justify-content-center justify-content-lg-start"
+              data-aos="fade-right"
+              data-aos-delay="100"
+            ></div>
+            <div
+              className="col-xl-7 ps-0 ps-lg-5 pe-lg-1 d-flex align-items-stretch"
+              data-aos="fade-left"
+              data-aos-delay="100"
+            >
+              <div className="content d-flex flex-column justify-content-center">
+                <h3>Voluptatem dignissimos provident quasi</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Duis aute irure dolor in reprehenderit
+                </p>
+                <div className="row">
+                  <div className="col-md-6 d-md-flex align-items-md-stretch">
+                    <div className="count-box">
+                      <i className="bi bi-emoji-smile"></i>
+                      <span
+                        data-purecounter-start="0"
+                        data-purecounter-end="65"
+                        data-purecounter-duration="2"
+                        className="purecounter"
+                      ></span>
+                      <p>
+                        <strong>Happy Clients</strong> consequuntur voluptas
+                        nostrum aliquid ipsam architecto ut.
+                      </p>
+                    </div>
                   </div>
-                </div>
 
-                <div className="col-md-6 d-md-flex align-items-md-stretch">
-                  <div className="count-box">
-                    <i className="bi bi-journal-richtext"></i>
-                    <span data-purecounter-start="0" data-purecounter-end="85" data-purecounter-duration="2" className="purecounter"></span>
-                    <p><strong>Projects</strong> adipisci atque cum quia aspernatur totam laudantium et quia dere tan</p>
+                  <div className="col-md-6 d-md-flex align-items-md-stretch">
+                    <div className="count-box">
+                      <i className="bi bi-journal-richtext"></i>
+                      <span
+                        data-purecounter-start="0"
+                        data-purecounter-end="85"
+                        data-purecounter-duration="2"
+                        className="purecounter"
+                      ></span>
+                      <p>
+                        <strong>Projects</strong> adipisci atque cum quia
+                        aspernatur totam laudantium et quia dere tan
+                      </p>
+                    </div>
                   </div>
-                </div>
 
-                <div className="col-md-6 d-md-flex align-items-md-stretch">
-                  <div className="count-box">
-                    <i className="bi bi-clock"></i>
-                    <span data-purecounter-start="0" data-purecounter-end="35" data-purecounter-duration="4" className="purecounter"></span>
-                    <p><strong>Years of experience</strong> aut commodi quaerat modi aliquam nam ducimus aut voluptate non vel</p>
+                  <div className="col-md-6 d-md-flex align-items-md-stretch">
+                    <div className="count-box">
+                      <i className="bi bi-clock"></i>
+                      <span
+                        data-purecounter-start="0"
+                        data-purecounter-end="35"
+                        data-purecounter-duration="4"
+                        className="purecounter"
+                      ></span>
+                      <p>
+                        <strong>Years of experience</strong> aut commodi quaerat
+                        modi aliquam nam ducimus aut voluptate non vel
+                      </p>
+                    </div>
                   </div>
-                </div>
 
-                <div className="col-md-6 d-md-flex align-items-md-stretch">
-                  <div className="count-box">
-                    <i className="bi bi-award"></i>
-                    <span data-purecounter-start="0" data-purecounter-end="20" data-purecounter-duration="4" className="purecounter"></span>
-                    <p><strong>Awards</strong> rerum asperiores dolor alias quo reprehenderit eum et nemo pad der</p>
+                  <div className="col-md-6 d-md-flex align-items-md-stretch">
+                    <div className="count-box">
+                      <i className="bi bi-award"></i>
+                      <span
+                        data-purecounter-start="0"
+                        data-purecounter-end="20"
+                        data-purecounter-duration="4"
+                        className="purecounter"
+                      ></span>
+                      <p>
+                        <strong>Awards</strong> rerum asperiores dolor alias quo
+                        reprehenderit eum et nemo pad der
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-
-      </div>
-    </section>
+      </section>
     </div>
-  )
+  );
 }
 
-export default Counts
+export default Counts;
--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -1,293 +1,209 @@
-import React from "react";
+//../assets/img/portfolio/portfolio-1.jpg
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
-import portfolio from "../assets/img/portfolio/portfolio-1.jpg";
+import portfolio_1 from "../assets/img/portfolio/portfolio-1.jpg";
+import portfolio_2 from "../assets/img/portfolio/portfolio-2.jpg";
+import portfolio_3 from "../assets/img/portfolio/portfolio-3.jpg";
+import portfolio_4 from "../assets/img/portfolio/portfolio-4.jpg";
+import portfolio_5 from "../assets/img/portfolio/portfolio-5.jpg";
+import portfolio_6 from "../assets/img/portfolio/portfolio-6.jpg";
+import portfolio_7 from "../assets/img/portfolio/portfolio-7.jpg";
+import portfolio_8 from "../assets/img/portfolio/portfolio-8.jpg";
+import { ImCancelCircle } from "react-icons/im";
+
+const Category = ({ dataSet }) => {
+  const [imageModal, setImageModal] = useState(false);
+  const [imageSource, setImageSource] = useState("");
+
+  const setImageOnModal = (el) => {
+    setImageModal(true);
+    setImageSource(el);
+  };
+
+  return (
+    <>
+      <div className={imageModal ? "image-box show-image-box" : "image-box"}>
+        <div className="image-box-content">
+          <Image src={imageSource} alt="" />
+          <span
+            className="image-box-close-btn"
+            onClick={() => setImageModal(false)}
+          >
+            <ImCancelCircle size={30} />
+          </span>
+        </div>
+      </div>
+      {dataSet.map((el) => {
+        return (
+          <div
+            className="col-lg-4 col-md-6 portfolio-item filter-app"
+            key={el._id}
+            onClick={() => setImageOnModal(el.image)}
+          >
+            <div className="portfolio-wrap">
+              <Image
+style={{height:"300px"}}
+                src={el.image}
+                className="img-fluid"
+                alt=""
+              />
+              <div className="portfolio-info">
+                <h4>{el.title}</h4>
+                <div className="portfolio-links">
+                  {/* <a
+                  href="../assets/img/portfolio/portfolio-1.jpg"
+                  data-gallery="portfolioGallery"
+                  className="portfolio-lightbox"
+                  title="App 1"
+                >
+                  <i className="bx bx-plus"></i>
+                </a> */}
+                  <a
+                    title="App 1"
+                    data-gallery="portfolio-gallery"
+                    className="glightbox preview-link"
+                  >
+                    <i
+                      className="bi bi-zoom-in"
+                      onClick={() => setImageOnModal(el.image)}
+                    ></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+const data = [
+  {
+    _id: "id" + Math.random().toString(16).slice(2),
+    title: "portfolio 1",
+    image: portfolio_1,
+    category: "comunik",
+  },
+  {
+    _id: "id" + Math.random().toString(16).slice(2),
+    title: "portfolio 2",
+    image: portfolio_2,
+    category: "comunik",
+  },
+  {
+    _id: "id" + Math.random().toString(16).slice(2),
+    title: "portfolio 3",
+    image: portfolio_3,
+    category: "unikvision",
+  },
+  {
+    _id: "id" + Math.random().toString(16).slice(2),
+    title: "portfolio 4",
+    image: portfolio_4,
+    category: "unikvision",
+  },
+  {
+    _id: "id" + Math.random().toString(16).slice(2),
+    title: "portfolio 5",
+    image: portfolio_5,
+    category: "unikespace",
+  },
+  {
+    _id: "id" + Math.random().toString(16).slice(2),
+    title: "portfolio 6",
+    image: portfolio_6,
+    category: "unikespace",
+  },
+  {
+    _id: "id" + Math.random().toString(16).slice(2),
+    title: "portfolio 7",
+    image: portfolio_7,
+    category: "unikvision",
+  },
+  {
+    _id: "id" + Math.random().toString(16).slice(2),
+    title: "portfolio 8",
+    image: portfolio_8,
+    category: "comunik",
+  },
+];
 function page() {
+  const [activeCategory, setActiveCategory] = useState("all");
+  const data_com = data.filter((el) => el.category === "comunik");
+  const data_espace = data.filter((el) => el.category === "unikvision");
+  const data_vision = data.filter((el) => el.category === "unikespace");
   return (
     <div>
-      <section id="portfolio" class="portfolio">
-        <div class="container" data-aos="fade-up">
-          <div class="section-title">
+      <section id="portfolio" className="portfolio">
+        <div className="container" data-aos="fade-up">
+          <div className="section-title" style={{ paddingTop: "100px" }}>
             <h2>Portfolio</h2>
             <p>Check our Portfolio</p>
           </div>
-          <div class="row" data-aos="fade-up" data-aos-delay="100">
-            <div class="col-lg-12 d-flex justify-content-center">
+          <div className="row" data-aos="fade-up" data-aos-delay="100">
+            <div className="col-lg-12 d-flex justify-content-center">
               <ul id="portfolio-flters">
-                <li data-filter="*" class="filter-active">
-                  All
+                <li
+                  data-filter="*"
+                  className={activeCategory === "all" ? "filter-active" : ""}
+                  data-bs-toggle="tab"
+                >
+                  <button onClick={() => setActiveCategory("all")}> All</button>
                 </li>
-                <li data-filter=".filter-app">Com Unik</li>
-                <li data-filter=".filter-card">Unik Vision</li>
-                <li data-filter=".filter-web">Espace Unik</li>
+                <li
+                  data-filter="*"
+                  className={
+                    activeCategory === "comunik" ? "filter-active" : ""
+                  }
+                  data-bs-toggle="tab"
+                >
+                  <button onClick={() => setActiveCategory("comunik")}>
+                    {" "}
+                    Com Unik
+                  </button>
+                </li>
+                <li
+                  data-filter="*"
+                  className={
+                    activeCategory === "unikespace" ? "filter-active" : ""
+                  }
+                  data-bs-toggle="tab"
+                >
+                  <button onClick={() => setActiveCategory("unikespace")}>
+                    {" "}
+                    Unik Espace
+                  </button>
+                </li>
+                <li
+                  data-filter="*"
+                  className={
+                    activeCategory === "unikvision" ? "filter-active" : ""
+                  }
+                  data-bs-toggle="tab"
+                >
+                  <button onClick={() => setActiveCategory("unikvision")}>
+                    {" "}
+                    Unik vision
+                  </button>
+                </li>
               </ul>
             </div>
           </div>
 
           <div
-            class="row portfolio-container"
+            className="row portfolio-container"
             data-aos="fade-up"
             data-aos-delay="200"
           >
-            <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-              <div class="portfolio-wrap">
-                <Image
-                  width={500}
-                  height={300}
-                  src={portfolio}
-                  class="img-fluid"
-                  alt=""
-                />
-                <div class="portfolio-info">
-                  <h4>App 1</h4>
-                  <p>App</p>
-                  <div class="portfolio-links">
-                    <a
-                      href="../assets/img/portfolio/portfolio-1.jpg"
-                      data-gallery="portfolioGallery"
-                      class="portfolio-lightbox"
-                      title="App 1"
-                    >
-                      <i class="bx bx-plus"></i>
-                    </a>
-                    <a href="portfolio-details.html" title="More Details">
-                      <i class="bx bx-link"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-              <div class="portfolio-wrap">
-                <Image
-                  width={500}
-                  height={300}
-                  src={portfolio}
-                  class="img-fluid"
-                  alt=""
-                />
-                <div class="portfolio-info">
-                  <h4>Web 3</h4>
-                  <p>Web</p>
-                  <div class="portfolio-links">
-                    <a
-                      href="../assets/img/portfolio/portfolio-2.jpg"
-                      data-gallery="portfolioGallery"
-                      class="portfolio-lightbox"
-                      title="Web 3"
-                    >
-                      <i class="bx bx-plus"></i>
-                    </a>
-                    <a href="portfolio-details.html" title="More Details">
-                      <i class="bx bx-link"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-              <div class="portfolio-wrap">
-                <Image
-                  width={500}
-                  height={300}
-                  src={portfolio}
-                  class="img-fluid"
-                  alt=""
-                />
-                <div class="portfolio-info">
-                  <h4>App 2</h4>
-                  <p>App</p>
-                  <div class="portfolio-links">
-                    <a
-                      href="../assets/img/portfolio/portfolio-3.jpg"
-                      data-gallery="portfolioGallery"
-                      class="portfolio-lightbox"
-                      title="App 2"
-                    >
-                      <i class="bx bx-plus"></i>
-                    </a>
-                    <a href="portfolio-details.html" title="More Details">
-                      <i class="bx bx-link"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-              <div class="portfolio-wrap">
-                <Image
-                  width={500}
-                  height={300}
-                  src={portfolio}
-                  class="img-fluid"
-                  alt=""
-                />
-                <div class="portfolio-info">
-                  <h4>Card 2</h4>
-                  <p>Card</p>
-                  <div class="portfolio-links">
-                    <a
-                      href="../assets/img/portfolio/portfolio-4.jpg"
-                      data-gallery="portfolioGallery"
-                      class="portfolio-lightbox"
-                      title="Card 2"
-                    >
-                      <i class="bx bx-plus"></i>
-                    </a>
-                    <a href="portfolio-details.html" title="More Details">
-                      <i class="bx bx-link"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-              <div class="portfolio-wrap">
-                <Image
-                  width={500}
-                  height={300}
-                  src={portfolio}
-                  class="img-fluid"
-                  alt=""
-                />
-                <div class="portfolio-info">
-                  <h4>Web 2</h4>
-                  <p>Web</p>
-                  <div class="portfolio-links">
-                    <a
-                      href="../assets/img/portfolio/portfolio-5.jpg"
-                      data-gallery="portfolioGallery"
-                      class="portfolio-lightbox"
-                      title="Web 2"
-                    >
-                      <i class="bx bx-plus"></i>
-                    </a>
-                    <a href="portfolio-details.html" title="More Details">
-                      <i class="bx bx-link"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-              <div class="portfolio-wrap">
-                <Image
-                  width={500}
-                  height={300}
-                  src={portfolio}
-                  class="img-fluid"
-                  alt=""
-                />
-                <div class="portfolio-info">
-                  <h4>App 3</h4>
-                  <p>App</p>
-                  <div class="portfolio-links">
-                    <a
-                      href="../assets/img/portfolio/portfolio-6.jpg"
-                      data-gallery="portfolioGallery"
-                      class="portfolio-lightbox"
-                      title="App 3"
-                    >
-                      <i class="bx bx-plus"></i>
-                    </a>
-                    <a href="portfolio-details.html" title="More Details">
-                      <i class="bx bx-link"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-              <div class="portfolio-wrap">
-                <Image
-                  width={500}
-                  height={300}
-                  src={portfolio}
-                  class="img-fluid"
-                  alt=""
-                />
-                <div class="portfolio-info">
-                  <h4>Card 1</h4>
-                  <p>Card</p>
-                  <div class="portfolio-links">
-                    <a
-                      href="../assets/img/portfolio/portfolio-7.jpg"
-                      data-gallery="portfolioGallery"
-                      class="portfolio-lightbox"
-                      title="Card 1"
-                    >
-                      <i class="bx bx-plus"></i>
-                    </a>
-                    <a href="portfolio-details.html" title="More Details">
-                      <i class="bx bx-link"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-              <div class="portfolio-wrap">
-                <Image
-                  width={500}
-                  height={300}
-                  src={portfolio}
-                  class="img-fluid"
-                  alt=""
-                />
-                <div class="portfolio-info">
-                  <h4>Card 3</h4>
-                  <p>Card</p>
-                  <div class="portfolio-links">
-                    <a
-                      href="../assets/img/portfolio/portfolio-8.jpg"
-                      data-gallery="portfolioGallery"
-                      class="portfolio-lightbox"
-                      title="Card 3"
-                    >
-                      <i class="bx bx-plus"></i>
-                    </a>
-                    <a href="portfolio-details.html" title="More Details">
-                      <i class="bx bx-link"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-              <div class="portfolio-wrap">
-                <Image
-                  width={500}
-                  height={300}
-                  src={portfolio}
-                  class="img-fluid"
-                  alt=""
-                />
-                <div class="portfolio-info">
-                  <h4>Web 3</h4>
-                  <p>Web</p>
-                  <div class="portfolio-links">
-                    <a
-                      href="../assets/img/portfolio/portfolio-9.jpg"
-                      data-gallery="portfolioGallery"
-                      class="portfolio-lightbox"
-                      title="Web 3"
-                    >
-                      <i class="bx bx-plus"></i>
-                    </a>
-                    <a href="portfolio-details.html" title="More Details">
-                      <i class="bx bx-link"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
+            {activeCategory === "all" && <Category dataSet={data} />}
+            {activeCategory === "comunik" && <Category dataSet={data_com} />}
+            {activeCategory === "unikvision" && (
+              <Category dataSet={data_vision} />
+            )}
+            {activeCategory === "unikespace" && (
+              <Category dataSet={data_espace} />
+            )}
           </div>
         </div>
       </section>
